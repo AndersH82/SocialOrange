797ba80f490b34ab4cdb5ae8137a5ff7fd0506ec
Anders Holst <andersthereseholst@gmail.com>
2024-03-20 10:57:14 +0000

    refactoring custom context

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://socialorangeapi-6ce409532368.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.baseURL = 'https://socialorangeapi-6ce409532368.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
