35098eeb0111f0de0dd972f5f5a4473f3b5f324a
Anders Holst <andersthereseholst@gmail.com>
2024-03-24 15:18:29 +0000

    add new url to axios baseurl

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://socialorangeapi-6ce409532368.herokuapp.com';
+axios.defaults.baseURL = 'https://8000-andersh82-drfworkspace-qu6lqgebckx.ws-eu110.gitpod.io/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
