521f17cf081219fd71b8925a204a519bf79eb440
Anders Holst <andersthereseholst@gmail.com>
2024-03-26 09:35:52 +0000

    removed console.logs

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://8000-andersh82-drfworkspace-qu6lqgebckx.ws-eu110.gitpod.io/';
+axios.defaults.baseURL = 'https://8000-andersh82-drfworkspace-me57xw0rdc4.ws-eu110.gitpod.io/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
